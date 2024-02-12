52e001d22ca3d5e74d8e2127fae8170b918fa7d1
Björn Tirsén <bjorn.tirsen@sbab.se>
2024-02-12 09:16:42 +0100

    03-components

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 import { createServer } from "http";
 import { readFile } from "fs/promises";
 import renderJSXToHTML from "./renderJSXToHTML.js";
+import PropTypes from "prop-types";
 
 createServer(async (req, res) => {
   const author = "Jae Doe";
   const postContent = await readFile("./posts/hello-world.txt", "utf8");
-  sendHTML(
-    res,
+  sendHTML(res, <BlogPostPage postContent={postContent} author={author} />);
+}).listen(8080);
+
+function sendHTML(res, jsx) {
+  const html = renderJSXToHTML(jsx);
+  res.setHeader("Content-Type", "text/html");
+  res.end(html);
+}
+
+BlogPostPage.propTypes = {
+  postContent: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+
+function BlogPostPage({ postContent, author }) {
+  return (
     <html lang="en">
       <head>
         <title>My blog</title>
@@ -17,21 +32,25 @@ createServer(async (req, res) => {
           <hr />
         </nav>
         <article>{postContent}</article>
-        <footer>
-          <hr />
-          <p>
-            <i>
-              (c) {author}, {new Date().getFullYear()}
-            </i>
-          </p>
-        </footer>
+        <Footer author={author} />
       </body>
     </html>
   );
-}).listen(8080);
+}
 
-function sendHTML(res, jsx) {
-  const html = renderJSXToHTML(jsx);
-  res.setHeader("Content-Type", "text/html");
-  res.end(html);
+Footer.propTypes = {
+  author: PropTypes.string.isRequired,
+};
+
+function Footer({ author }) {
+  return (
+    <footer>
+      <hr />
+      <p>
+        <i>
+          (c) {author} {new Date().getFullYear()}
+        </i>
+      </p>
+    </footer>
+  );
 }
